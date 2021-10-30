a88edd9365e65907217f5109ec333d6a45e0623f
Carlos Correa <33291776+Deadflight@users.noreply.github.com>
2021-10-30 14:46:59 -0400

    getStaticProps Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,34 @@ import React, { useState, useEffect } from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
+import fetch from 'isomorphic-unfetch'
 
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+// export const getServerSideProps = async () => { // use on page only
+//   const response = await fetch('https://avocado-nextjs.vercel.app/api/avo');
+//   const { data: productList }: TAPIAvoResponse = await response.json();
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+//   return {
+//     props: {
+//       productList
+//     }
+//   }
+// }
+
+// Static Generation
+
+export const getStaticProps = async () => { // use on page only, only for production
+  const response = await fetch('https://avocado-nextjs.vercel.app/api/avo');
+  const { data: productList }: TAPIAvoResponse = await response.json();
+
+  return {
+    props: {
+      productList
+    }
+  }
+}
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
 
   return (
     <Layout>
